b0cc71fa5399a465bc5994dfb8e2d59851ab41b5
JayaDarby <jayadarby@gmail.com>
2015-09-18 04:00:50 -0700

    added facebook auth & homepage styling

--- garage_sale_app/config/application.rb
+++ garage_sale_app/config/application.rb
@@ -9,6 +9,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
+
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -31,5 +32,7 @@ module GarageSaleApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.enabled = true  
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"  
   end
 end
