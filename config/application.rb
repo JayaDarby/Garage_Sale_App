89b2daf328331fe81d4dc48d71555c52f6aaf8e5
JayaDarby <jayadarby@gmail.com>
2015-09-30 00:28:40 -0700

    hoping this works

--- config/application.rb
+++ config/application.rb
@@ -15,12 +15,12 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false 
+
 
 
 module GarageSaleApp
   class Application < Rails::Application
-
+    config.assets.initialize_on_precompile = false 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -37,6 +37,5 @@ module GarageSaleApp
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.enabled = true  
     config.assets.paths << "#{Rails.root}/app/assets/fonts" 
-    config.assets.initialize_on_precompile = false 
   end
 end
