697c37d4ba1f2fe0b91bd32a29335361ccb6f1f2
JayaDarby <jayadarby@gmail.com>
2015-09-29 23:07:15 -0700

    adding stuff

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,7 @@ module GarageSaleApp
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.enabled = true  
-    config.assets.paths << "#{Rails.root}/app/assets/fonts"  
+    config.assets.paths << "#{Rails.root}/app/assets/fonts" 
+    config.assets.initialize_on_precompile = false 
   end
 end
